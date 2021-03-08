8693cb522f1fec3e13066c102e8e592096563824
boury_a <boury_a@etna-alternance.net>
2021-03-08 16:43:11 +0100

    fixed

--- main.py
+++ main.py
@@ -6,26 +6,27 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--password", help="length of the generated password", type=int)
-    parser.add_argument("--name", help="purpose of the password generation", type=str)
+    parser.add_argument("--purpose", help="purpose of the password generation", type=str)
     parser.add_argument("--file", help="file where password will be saved", type=str)
     parser.add_argument("--token", help="handle the token, deletion request => true or pass", type=str)
-    parser.add_argument("--ckeck",
+    parser.add_argument("--check",
                         help="check if the password you seek is correct compare to the hashed version => true or pass, type=str")
     args = parser.parse_args()
     if args.password:
         psswd = password_generation(args.password)
-        hashed = crypt(save_crypt_key("salt.txt"), "password.txt", psswd, str(args.name))
+        hashed = crypt(save_crypt_key("salt.txt"), str(args.file), psswd, str(args.purpose))
         check(psswd, hashed)
     if args.token == "true":
         display_token("salt.txt")
-    if args.decrypt == "true":
-        if args.name:
+    if args.check == "true":
+        if args.purpose:
             if args.file:
-                compare_hash(str(args.file), str(args.name))
+                compare_hash(str(args.file), str(args.purpose))
             else:
-                print("You need at least a purpose and a filename to decrypt a password")
-
+                print("You need at least a purpose(--purpose) and a filename(--file) to decrypt a password")
         else:
             print("You need at least a purpose and a filename to decrypt a password")
+    if args.check == "false":
+        pass
     else:
-        print("ERROR: {} is an invalid parameter for arg --decrypt".format(args.decrypt))
+        print("ERROR: {} is an invalid parameter for arg --check".format(args.check))
--- password.py
+++ password.py
@@ -2,6 +2,8 @@ import string
 import random
 import bcrypt
 import re
+import os.path
+from os import path
 
 
 def password_generation(l):
@@ -10,30 +12,33 @@ def password_generation(l):
     :param l: password length
     :return: byte object
     """
-    try:
-        letters = string.ascii_letters
-        numbers = string.digits
-        punctuation = string.punctuation
+    # try:
+    letters = string.ascii_letters
+    numbers = string.digits
+    punctuation = string.punctuation
 
-        want_digits = bool(input("Want digits ? (Enter anything or leave empty if not): "))
-        want_letters = bool(input("Want letters ? (Enter anything or leave empty if not): "))
-        want_puncts = bool(input("Want punctuation ? (Enter anything or leave empty if not): "))
-        if not want_digits:
-            numbers = ""
-        if not want_letters:
-            letters = ""
-        if not want_puncts:
-            punctuation = ""
-        ascii_caracters = f'{letters}{numbers}{punctuation}'
-        assert ascii_caracters != ""
-        ascii_caracters = list(ascii_caracters)
-        random.shuffle(ascii_caracters)
-        random_password = random.choices(ascii_caracters, weights=None, k=l)
-        random_password = ''.join(random_password)
-        print(random_password.encode())
-        return random_password.encode()
-    except Exception:
-        print(" Oops something went wrong!")
+    want_digits = bool(
+        input("Want digits ? (Enter anything or leave empty if not): "))
+    want_letters = bool(
+        input("Want letters ? (Enter anything or leave empty if not): "))
+    want_puncts = bool(
+        input("Want punctuation ? (Enter anything or leave empty if not): "))
+    if not want_digits:
+        numbers = ""
+    if not want_letters:
+        letters = ""
+    if not want_puncts:
+        punctuation = ""
+    ascii_caracters = f'{letters}{numbers}{punctuation}'
+    assert ascii_caracters != ""
+    ascii_caracters = list(ascii_caracters)
+    random.shuffle(ascii_caracters)
+    random_password = random.choices(ascii_caracters, weights=None, k=l)
+    random_password = ''.join(random_password)
+    print(random_password.encode())
+    return random_password.encode()
+    # except Exception:
+    #     print(" Oops something went wrong!")
 
 
 def save_crypt_key(filename):
@@ -42,13 +47,16 @@ def save_crypt_key(filename):
     :param filename:
     :return: byte object
     """
-    with open(filename, "r+") as file:
-        if len(file.read(1)) > 0:
-            key = input("Enter the key: ")
-            return key.encode()
-        salt = bcrypt.gensalt()
-        file.write(str(salt))
-        return salt
+    if not path.exists(filename):
+        open(filename, 'w').close()
+    else:
+        with open(filename, "r+") as file:
+            if len(file.read(1)) > 0:
+                key = input("Enter the key: ")
+                return key.encode()
+            salt = bcrypt.gensalt()
+            file.write(str(salt))
+            return salt
 
 
 def crypt(crypt_key, filename2, passwd, purpose):
@@ -61,11 +69,15 @@ def crypt(crypt_key, filename2, passwd, purpose):
     :return: byte object
     """
     hashed = bcrypt.hashpw(passwd, crypt_key)
-    with open(filename2, "a") as file2:
-        print(hashed)
-        print(type(hashed))
-        file2.write(purpose + ': ' + str(hashed) + '\n')
-        return hashed
+    with open(filename2) as file:
+        if not purpose in file.read():
+            with open(filename2, "a") as file2:
+                print("hashed: " + hashed.decode())
+                file2.write(purpose + ': ' + str(hashed) + '\n')
+                return hashed
+    if purpose in file.read():
+        print("FAILURE you cannot have two password for indentical purpose!!!")
+        exit()
 
 
 def check(passwd, hashed_password):
@@ -76,7 +88,7 @@ def check(passwd, hashed_password):
     :return:
     """
     if bcrypt.checkpw(passwd, hashed_password):
-        print("SUCCESS, this is your crypted password: " + passwd.decode())
+        print("SUCCESS, this is your crypted password:" + passwd.decode())
     else:
         print("FAILURE, the password remains hashed " + hashed_password.decode())
 
@@ -90,7 +102,8 @@ def display_token(filename1):
     with open(filename1, "r+") as file:
         crypt_key = file.readline()
         print("THE TOKEN: " + crypt_key)
-        erase = bool(input("Do you want to erase the file? Enter anything or pass for NO: "))
+        erase = bool(
+            input("Do you want to erase the file? Enter anything or pass for NO: "))
         if erase:
             file.truncate(0)
         return
@@ -100,12 +113,15 @@ def compare_hash(filename2, pattern):
     with open(filename2, "r") as file:
         for l in file:
             if pattern in l:
-                display_hashed = l[len(pattern) + 3:-1]
-                hashed = l[len(pattern):].encode()
-        print(display_hashed)
-        passw = input("Entry the password corresponding to the website or application in clear:")
-        passw = passw.encode()
-        if bcrypt.hashpw(passw, hashed):
-            return "Correct Password"
-        else:
-            return "Incorrect Password"
+                hashed = l[len(pattern) + 4:-1]
+                hashed = hashed.encode()
+                #print("hashed => => =>"+str(hashed))
+                passw = input(
+                    "Entry the password corresponding to the website or application in str:")
+                passw=passw.encode()
+                if bcrypt.hashpw(passw, hashed):
+                    print("Correct Password!")
+                    return True
+                else:
+                    print("Incorrect Password!")
+                    return False
